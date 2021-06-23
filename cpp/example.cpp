002ffc3051a7ac17f34fa7a9c9bf0d7b8c8cc0a7
ammarahm-ed <ammarahmed6506@gmail.com>
2021-06-23 19:58:39 +0500

    refactor c++ code

--- cpp/example.cpp
+++ cpp/example.cpp
@@ -1,7 +1,68 @@
 #include "example.h"
+#include <jsi/jsi.h>
+
+using namespace facebook::jsi;
+using namespace std;
 
 namespace example {
-	int multiply(float a, float b) {
-		return a * b;
-	}
+
+void install(Runtime &jsiRuntime) {
+    
+    auto helloWorld = Function::createFromHostFunction(jsiRuntime,
+                                                       PropNameID::forAscii(jsiRuntime,
+                                                                            "helloWorld"),
+                                                       0,
+                                                       [](Runtime &runtime,
+                                                          const Value &thisValue,
+                                                          const Value *arguments,
+                                                          size_t count) -> Value {
+        string helloworld = "helloworld";
+        
+        
+        return Value(runtime,
+                     String::createFromUtf8(
+                                            runtime,
+                                            helloworld));
+        
+    });
+    
+    jsiRuntime.global().setProperty(jsiRuntime, "helloWorld", move(helloWorld));
+    
+    auto multiply = Function::createFromHostFunction(jsiRuntime,
+                                                     PropNameID::forAscii(jsiRuntime,
+                                                                          "multiply"),
+                                                     2,
+                                                     [](Runtime &runtime,
+                                                        const Value &thisValue,
+                                                        const Value *arguments,
+                                                        size_t count) -> Value {
+        int x = arguments[0].getNumber();
+        int y = arguments[1].getNumber();
+        
+        return Value(x * y);
+        
+    });
+    
+    jsiRuntime.global().setProperty(jsiRuntime, "multiply", move(multiply));
+    
+    auto multiplyWithCallback = Function::createFromHostFunction(jsiRuntime,
+                                                                 PropNameID::forAscii(jsiRuntime,
+                                                                                      "multiplyWithCallback"),
+                                                                 3,
+                                                                 [](Runtime &runtime,
+                                                                    const Value &thisValue,
+                                                                    const Value *arguments,
+                                                                    size_t count) -> Value {
+        int x = arguments[0].getNumber();
+        int y = arguments[1].getNumber();
+        
+        arguments[2].getObject(runtime).getFunction(runtime).call(runtime, x * y);
+        
+        return Value();
+        
+    });
+    
+    jsiRuntime.global().setProperty(jsiRuntime, "multiplyWithCallback", move(multiplyWithCallback));
+}
+
 }
--- cpp/example.h
+++ cpp/example.h
@@ -1,37 +1,13 @@
 #ifndef EXAMPLE_H
 #define EXAMPLE_H
 
-#include <jsi/jsi.h>
-
-using namespace facebook;
-using namespace jsi;
-using namespace std;
+namespace jsi {
+class Runtime;
+}
 
 namespace example {
 
-void install(Runtime &jsiRuntime) {
-    
-    auto helloWorld = Function::createFromHostFunction(jsiRuntime,
-                                                       PropNameID::forAscii(jsiRuntime,
-                                                                            "helloWorld"),
-                                                       0,
-                                                       [](Runtime &runtime,
-                                                          const Value &thisValue,
-                                                          const Value *arguments,
-                                                          size_t count) -> Value {
-        
-        string helloworld = "helloworld";
-        
-        
-        return Value(runtime,
-                     String::createFromUtf8(
-                                            runtime,
-                                            helloworld));
-        
-    });
-    
-    jsiRuntime.global().setProperty(jsiRuntime, "helloWorld", move(helloWorld));
-}
+void install(Runtime &jsiRuntime);
 
 }
 
