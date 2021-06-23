48127da6390acf5fed066f3196290c001c29fb57
ammarahm-ed <ammarahmed6506@gmail.com>
2021-06-23 21:12:30 +0500

    delete everything in index.tsx

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type SimpleJsiType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { SimpleJsi } = NativeModules;
-
-export default SimpleJsi as SimpleJsiType;
