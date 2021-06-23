0cf389dfbfdf6108983cf9cbaf746712e84b6b87
ammarahm-ed <ammarahmed6506@gmail.com>
2021-06-23 19:33:55 +0500

    change App.tsx to App.js

--- example/src/App.js
+++ example/src/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [result, setResult] = React.useState();
 
   React.useEffect(() => {
     setResult(global.helloWorld())
