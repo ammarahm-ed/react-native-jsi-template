238dfc523833f8e774e5d0ad1ec9308ecf50c47c
Alexander Pataridze <s.pataridze@gmail.com>
2021-12-26 20:51:34 +0400

    map algos

--- cpp/example.cpp
+++ cpp/example.cpp
@@ -17,25 +17,6 @@ namespace example {
 
     void install(Runtime &jsiRuntime) {
 
-        auto helloWorld = Function::createFromHostFunction(jsiRuntime,
-                                                           PropNameID::forAscii(jsiRuntime,
-                                                                                "helloWorld"), 0,
-                                                           [](Runtime &runtime,
-                                                              const Value &thisValue,
-                                                              const Value *arguments,
-                                                              size_t count) -> Value {
-                                                               string helloworld = "helloworld";
-
-
-                                                               return Value(runtime,
-                                                                            String::createFromUtf8(
-                                                                                    runtime,
-                                                                                    helloworld));
-
-                                                           });
-
-        jsiRuntime.global().setProperty(jsiRuntime, "helloWorld", move(helloWorld));
-
         auto factorial = Function::createFromHostFunction(jsiRuntime,
                                                           PropNameID::forAscii(jsiRuntime,
                                                                                "factorial"), 1,
@@ -50,22 +31,6 @@ namespace example {
 
         jsiRuntime.global().setProperty(jsiRuntime, "factorial", move(factorial));
 
-
-        auto loop = Function::createFromHostFunction(jsiRuntime,
-                                                     PropNameID::forAscii(jsiRuntime,
-                                                                          "loop"), 1,
-                                                     [](Runtime &runtime,
-                                                        const Value &thisValue,
-                                                        const Value *arguments,
-                                                        size_t count) -> Value {
-                                                         int num = arguments[0].getNumber();
-                                                         for (int i = 0; i < num; i++) {
-
-                                                         }
-                                                         return Value(num);
-                                                     });
-
-        jsiRuntime.global().setProperty(jsiRuntime, "loop", move(loop));
 
         auto getOneByPi = Function::createFromHostFunction(jsiRuntime,
                                                            PropNameID::forAscii(jsiRuntime,
@@ -133,46 +98,6 @@ namespace example {
 
         jsiRuntime.global().setProperty(jsiRuntime, "gaussLegendre", move(gaussLegendre));
 
-        auto multiply = Function::createFromHostFunction(jsiRuntime,
-                                                         PropNameID::forAscii(jsiRuntime,
-                                                                              "multiply"),
-                                                         2,
-                                                         [](Runtime &runtime,
-                                                            const Value &thisValue,
-                                                            const Value *arguments,
-                                                            size_t count) -> Value {
-                                                             int x = arguments[0].getNumber();
-                                                             int y = arguments[1].getNumber();
-
-                                                             return Value(x * y);
-
-                                                         });
-
-        jsiRuntime.global().setProperty(jsiRuntime, "multiply", move(multiply));
-
-        auto multiplyWithCallback = Function::createFromHostFunction(jsiRuntime,
-                                                                     PropNameID::forAscii(
-                                                                             jsiRuntime,
-                                                                             "multiplyWithCallback"),
-                                                                     3,
-                                                                     [](Runtime &runtime,
-                                                                        const Value &thisValue,
-                                                                        const Value *arguments,
-                                                                        size_t count) -> Value {
-                                                                         int x = arguments[0].getNumber();
-                                                                         int y = arguments[1].getNumber();
-
-                                                                         arguments[2].getObject(
-                                                                                 runtime).getFunction(
-                                                                                 runtime).call(
-                                                                                 runtime, x * y);
-
-                                                                         return Value();
-
-                                                                     });
-
-        jsiRuntime.global().setProperty(jsiRuntime, "multiplyWithCallback",
-                                        move(multiplyWithCallback));
     }
 
 }
--- example/App.js
+++ example/App.js
@@ -1,108 +1,99 @@
-/* eslint-disable no-script-url */
-/* eslint-disable no-undef */
-import React, {useEffect, useRef, useState} from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
 import jsi from 'react-native-jsi-template';
-import Timer from './timer';
 
-const ITERATATION_COUNT = 1000000;
+const benchmarks = [
+  {
+    name: 'Borwein',
+    iterations: 1000,
+    precision: 100,
+    method: jsi.getOneByPi,
+  },
+  {
+    name: 'GaussLegendre',
+    iterations: 1000,
+    precision: 100,
+    method: jsi.gaussLegendre,
+  },
+  {
+    name: 'RecursiveFactorial',
+    iterations: 1000,
+    precision: 15,
+    method: jsi.factorial,
+  },
+];
 
-function jsFactorial(n) {
-  if (n > 1) return n * jsFactorial(n - 1);
-  return 1;
-}
-
-function jsGaussLegendre(iterations) {
-  let a = 1.0;
-  let b = 1.0 / Math.sqrt(2);
-  let t = 1.0 / 4.0;
-  let p = 1.0;
-  for (let i = 0; i < iterations; i++) {
-    let aNext = (a + b) / 2;
-    let bNext = Math.sqrt(a * b);
-    let tNext = t - p * Math.pow(a - aNext, 2);
-    let pNext = 2 * p;
-    a = aNext;
-    b = bNext;
-    t = tNext;
-    p = pNext;
-  }
-  return Math.pow(a + b, 2) / (4 * t);
-}
-
-function jsLoop(iterations) {
-  for (let i = 0; i < iterations; i++) {}
-  return iterations;
-}
-
-function jsGetOneByPi(k) {
-  let ak = 6.0 - 4 * Math.sqrt(2);
-  let yk = Math.sqrt(2) - 1.0;
-  let ak1;
-  let yk1;
-  for (let i = 0; i < k; i++) {
-    yk1 =
-      (1 - Math.pow(1 - yk * yk * yk * yk, 0.25)) /
-      (1 + Math.pow(1 - yk * yk * yk * yk, 0.25));
-    ak1 =
-      ak * Math.pow(1 + yk1, 4) -
-      Math.pow(2, 2 * i + 3) * yk1 * (1 + yk1 + yk1 * yk1);
-    yk = yk1;
-    ak = ak1;
-  }
-  return ak;
-}
+const EMPTY_RESULT = {
+  result: '',
+  duration: '',
+};
 
 export default function App() {
-  const [resultsObj, setResultsObj] = useState({
-    js: {duration: 0, result: 0},
-    native: {duration: 0, result: 0},
+  const [benchmarkResults, setBenchmarkResults] = useState({
+    Borwein: {
+      ...EMPTY_RESULT,
+    },
+    GaussLegendre: {
+      ...EMPTY_RESULT,
+    },
+    RecursiveFactorial: {
+      ...EMPTY_RESULT,
+    },
   });
+  const benchmarkResultsRef = useRef(benchmarkResults);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.algo}>
-        Algo: Basic Loop (Iterations: {ITERATATION_COUNT})
-      </Text>
-      <Text style={styles.title}>Native:</Text>
-      <Text>Result: {resultsObj.native.result}</Text>
-      <Text>Duration: {resultsObj.native.duration}</Text>
-      <Text style={styles.title}>JavaScript:</Text>
-      <Text>Result: {resultsObj.js.result}</Text>
-      <Text>Duration: {resultsObj.js.duration}</Text>
-      <TouchableOpacity
-        onPress={() => {
-          const jsPerf = performance.now();
-          const jsResValue = jsLoop(ITERATATION_COUNT);
-          const jsPerfRes = performance.now() - jsPerf;
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <TouchableOpacity
+          onPress={() => {
+            benchmarks.forEach(({method, name, iterations, precision}) => {
+              const perfNow = performance.now();
+              const result = method(precision);
 
-          const nativePerf = performance.now();
-          const nativeResValue = jsi.loop(ITERATATION_COUNT);
-          const nativePerfRes = performance.now() - nativePerf;
+              for (let i = 1; i < iterations; i++) {
+                method(precision);
+              }
 
-          setResultsObj({
-            native: {
-              duration: nativePerfRes,
-              result: nativeResValue,
-            },
-            js: {
-              duration: jsPerfRes,
-              result: jsResValue,
-            },
-          });
-        }}
-        style={styles.button}>
-        <Text style={styles.buttonTxt}>Start calculation</Text>
-      </TouchableOpacity>
-    </View>
+              const _benchmarkResults = {...benchmarkResultsRef.current};
+              _benchmarkResults[name] = {
+                result,
+                duration: performance.now() - perfNow,
+              };
+              benchmarkResultsRef.current = _benchmarkResults;
+              setBenchmarkResults(_benchmarkResults);
+            });
+          }}
+          style={styles.button}>
+          <Text style={styles.buttonTxt}>Start benchmarks</Text>
+        </TouchableOpacity>
+        {benchmarks.map(({name, iterations, precision, method}, index) => (
+          <View style={styles.item} key={name}>
+            <Text style={styles.algo}>Algo: {name}</Text>
+            <Text>Iterations: {iterations}</Text>
+            <Text>Precision: {precision}</Text>
+
+            <Text>Result: {benchmarkResults[name].result}</Text>
+            <Text>Duration: {benchmarkResults[name].duration}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: 'white',
+    marginHorizontal: 20,
   },
   title: {
     fontSize: 16,
@@ -114,7 +105,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   button: {
-    width: '95%',
+    width: '100%',
     alignSelf: 'center',
     height: 40,
     backgroundColor: 'blue',
@@ -125,5 +116,8 @@ const styles = StyleSheet.create({
   },
   buttonTxt: {
     color: 'white',
+  },
+  item: {
+    marginTop: 20,
   },
 });
